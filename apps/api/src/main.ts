90ad2b45d8ce55255bb4b37b889825c13174cb2b
uplooq <uplooq13@gmail.com>
2026-03-10 14:32:08 +0300

    backend update

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors({
     origin: process.env.FRONTEND_URL ?? 'http://localhost:3000',
